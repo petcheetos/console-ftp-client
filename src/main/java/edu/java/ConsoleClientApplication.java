4c8e8d056292e225fd2da090b89ba7f38a92e395
Anastasia <whydema@gmail.com>
2024-07-24 13:02:42 +0300

    refactor

--- src/main/java/edu/java/ConsoleClientApplication.java
+++ src/main/java/edu/java/ConsoleClientApplication.java
@@ -13,7 +13,6 @@ import java.util.HashSet;
 
 public class ConsoleClientApplication {
     private boolean passiveMode = true;
-
     private final ConsoleInterface console;
 
     public ConsoleClientApplication() {
@@ -27,50 +26,58 @@ public class ConsoleClientApplication {
             console.print(ConsoleMessages.ACTIVE_MODE);
         }
 
-        console.print(ConsoleMessages.INPUT_IP);
-        String ip = console.readLine();
-
-        console.print(ConsoleMessages.INPUT_LOGIN);
-        String login = console.readLine();
-
-        console.print(ConsoleMessages.INPUT_PASSWORD);
-        String password = console.readLine();
+        String ip = getInput(ConsoleMessages.INPUT_IP);
+        String login = getInput(ConsoleMessages.INPUT_LOGIN);
+        String password = getInput(ConsoleMessages.INPUT_PASSWORD);
 
         FTPClient ftpClient = new FTPClient(ip, login, password);
         JSONHandler jsonHandler = new JSONHandler();
 
         if (ftpClient.connect()) {
             console.print(ConsoleMessages.CONNECTED);
-            console.print(ConsoleMessages.INPUT_FILE_NAME);
-            String remoteFilePath = console.readLine();
-            String data = ftpClient.getDataFromFile(remoteFilePath, passiveMode);
-            while (data == null) {
-                console.print(ConsoleMessages.INVALID_FILENAME);
-                remoteFilePath = console.readLine();
-                data = ftpClient.getDataFromFile(remoteFilePath, passiveMode);
-            }
-            HashSet<Student> students = jsonHandler.parseData(data);
-            StudentService studentService = new StudentService(students);
-            Commands commands = new Commands(studentService);
-            CommandHandler commandHandler = new CommandHandler(console, commands);
-
-            while (true) {
-                console.print(commands.help());
-                String command = console.readLine();
-                if (commandHandler.handleCommand(command)) {
-                    String dataToFile = jsonHandler.serializeStudents(students);
-                    if (ftpClient.saveDataToFile(dataToFile, remoteFilePath, passiveMode)) {
-                        console.print(ConsoleMessages.DATA_SAVED);
-                    } else {
-                        console.print(ConsoleMessages.NO_DATA_SAVED);
-                    }
-                    ftpClient.disconnect();
-                    break;
-                }
-            }
+            String remoteFilePath = getValidFilePath(ftpClient);
+            HashSet<Student> students = jsonHandler.parseData(ftpClient.getDataFromFile(remoteFilePath, passiveMode));
+            handleCommands(ftpClient, jsonHandler, students, remoteFilePath);
         } else {
             console.printError(ConsoleMessages.NOT_CONNECTED);
         }
         console.close();
+    }
+
+    private String getInput(String message) {
+        console.print(message);
+        return console.readLine();
+    }
+
+    private String getValidFilePath(FTPClient ftpClient) {
+        String remoteFilePath = getInput(ConsoleMessages.INPUT_FILE_NAME);
+        String data = ftpClient.getDataFromFile(remoteFilePath, passiveMode);
+        while (data == null) {
+            console.print(ConsoleMessages.INVALID_FILENAME);
+            remoteFilePath = console.readLine();
+            data = ftpClient.getDataFromFile(remoteFilePath, passiveMode);
+        }
+        return remoteFilePath;
+    }
+
+    private void handleCommands(FTPClient ftpClient, JSONHandler jsonHandler, HashSet<Student> students, String remoteFilePath) {
+        StudentService studentService = new StudentService(students);
+        Commands commands = new Commands(studentService);
+        CommandHandler commandHandler = new CommandHandler(console, commands);
+
+        while (true) {
+            console.print(commands.help());
+            String command = console.readLine();
+            if (commandHandler.handleCommand(command)) {
+                String dataToFile = jsonHandler.serializeStudents(students);
+                if (ftpClient.saveDataToFile(dataToFile, remoteFilePath, passiveMode)) {
+                    console.print(ConsoleMessages.DATA_SAVED);
+                } else {
+                    console.print(ConsoleMessages.NO_DATA_SAVED);
+                }
+                ftpClient.disconnect();
+                break;
+            }
+        }
     }
 }
